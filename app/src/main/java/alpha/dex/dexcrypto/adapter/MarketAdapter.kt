cf0db94739b6d714a5a1a88371af0b5f5a7738ec
Devesh Pandey <72290650+deveshp007@users.noreply.github.com>
2022-10-24 10:00:03 +0530

    added MarketAdapter.kt & initialized Views in TopGainLoseFragment.kt

---
+++ app/src/main/java/alpha/dex/dexcrypto/adapter/MarketAdapter.kt
@@ -0,0 +1,59 @@
+package alpha.dex.dexcrypto.adapter
+
+import alpha.dex.dexcrypto.R
+import alpha.dex.dexcrypto.databinding.CurrencyItemLayoutBinding
+import alpha.dex.dexcrypto.model.CryptoCurrency
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class MarketAdapter(var context: Context, val list: List<CryptoCurrency>) :
+    RecyclerView.Adapter<MarketAdapter.MarketViewHolder>() {
+
+    inner class MarketViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var binding = CurrencyItemLayoutBinding.bind(view)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MarketViewHolder {
+        return MarketViewHolder(
+            LayoutInflater.from(context).inflate(R.layout.currency_item_layout, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: MarketViewHolder, position: Int) {
+        val item = list[position]
+        holder.binding.currencyNameTextView.text = item.name
+        holder.binding.currencySymbolTextView.text = item.symbol
+
+        Glide.with(context).load(
+            "https://s2.coinmarketcap.com/static/img/coins/64x64/" + item.id + ".png"
+        ).thumbnail(Glide.with(context).load(R.drawable.spinner))
+            .into(holder.binding.currencyImageView)
+
+        Glide.with(context).load(
+            "https://s3.coinmarketcap.com/generated/sparklines/web/7d/usd/" + item.id + ".png"
+        ).thumbnail(Glide.with(context).load(R.drawable.spinner))
+            .into(holder.binding.currencyChartImageView)
+
+        holder.binding.currencyPriceTextView.text =
+            "$ ${String.format("%.02f", item.quotes[0].price)}"
+
+
+        if (item.quotes!![0].percentChange24h > 0) {
+            holder.binding.currencyChangeTextView.setTextColor(context.resources.getColor(R.color.green))
+            holder.binding.currencyChangeTextView.text =
+                "+${String.format("%.02f", item.quotes[0].percentChange24h)} %"
+        } else {
+            holder.binding.currencyChangeTextView.setTextColor(context.resources.getColor(R.color.red))
+            holder.binding.currencyChangeTextView.text =
+                "${String.format("%.02f", item.quotes[0].percentChange24h)} %"
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+}
--- app/src/main/java/alpha/dex/dexcrypto/ui/fragment/TopGainLoseFragment.kt
+++ app/src/main/java/alpha/dex/dexcrypto/ui/fragment/TopGainLoseFragment.kt
@@ -6,15 +6,75 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import alpha.dex.dexcrypto.R
+import alpha.dex.dexcrypto.adapter.MarketAdapter
+import alpha.dex.dexcrypto.api.ApiInterface
+import alpha.dex.dexcrypto.api.ApiUtilities
+import alpha.dex.dexcrypto.databinding.FragmentTopGainLoseBinding
+import alpha.dex.dexcrypto.model.CryptoCurrency
+import android.view.View.GONE
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class TopGainLoseFragment : Fragment() {
+
+    lateinit var binding: FragmentTopGainLoseBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_top_gain_lose, container, false)
+        binding = FragmentTopGainLoseBinding.inflate(layoutInflater)
+
+        getMarketData()
+
+        return binding.root
+    }
+
+    private fun getMarketData() {
+
+        val position = requireArguments().getInt("position")
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            val res = ApiUtilities.getInstance().create(ApiInterface::class.java).getMarketData()
+
+            if (res.body() != null) {
+                withContext(Dispatchers.Main) {
+                    val dataItem = res.body()!!.data.cryptoCurrencyList
+
+                    Collections.sort(dataItem) { o1, o2 ->
+                        (o2.quotes[0].percentChange24h.toInt())
+                            .compareTo(o1.quotes[0].percentChange24h.toInt())
+                    }
+
+                    binding.spinKitView.visibility = GONE
+
+                    val list = ArrayList<CryptoCurrency>()
+
+                    if (position == 0) {
+                        list.clear()
+                        for (i in 0..9) {
+                            list.add(dataItem[i])
+                        }
+                        binding.topGainLoseRecyclerView.adapter =
+                            MarketAdapter(requireContext(), list)
+
+                    } else {
+                        list.clear()
+                        for (i in 0..9) {
+                            list.add(dataItem[dataItem.size - 1 - i])
+                        }
+                        binding.topGainLoseRecyclerView.adapter =
+                            MarketAdapter(requireContext(), list)
+
+                    }
+                }
+            }
+        }
     }
 }
